01d8ab10b4318f431b2d165cf6f55bde443a7bb7
Eulga <kimyeoun10@gmail.com>
2023-06-24 23:53:16 +0900

    level1 조건 완료

--- src/main/java/sparta/sparta_spring_level_1/dto/PostResponse.java
+++ src/main/java/sparta/sparta_spring_level_1/dto/PostResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import sparta.sparta_spring_level_1.entity.Post;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class PostResponse {
@@ -11,11 +13,15 @@ public class PostResponse {
     private String title;
     private String content;
     private String author;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public PostResponse(Post post) {
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
         this.author = post.getAuthor();
+        this.createdAt = post.getCreatedAt();
+        this.modifiedAt = post.getModifiedAt();
     }
 }
